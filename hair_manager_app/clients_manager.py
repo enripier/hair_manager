bc2d5ed8112715914d091997349ae0888aed9e83
enripier <hp.chibi@gmail.com>
2025-06-20 22:08:21 +0200

    Refactor: extract generic contact modification/ deletion from client functions

--- hair_manager_app/clients_manager.py
+++ hair_manager_app/clients_manager.py
@@ -1,10 +1,12 @@
+from rich.table import Table
+from rich.console import Console
+
 from . import constantes as c
-from . import app_interface as interface
+from . import contacts_manager as contacts
 from . import data_manager as data
 from . import app_load as load
 
-from rich.table import Table
-from rich.console import Console
+
 
 def add_client(client_database):
     """
@@ -47,112 +49,22 @@ def add_client(client_database):
     return client_database
 
 def modify_client(client_database):
-    """
-    Modifies single client in the client database
+    """_summary_Modifies single client in the client database
 
     Args:
         client_database (list of dict): Client database
-    
-    Returns:
-        list of dict: Updated client database
     """
-
-    # Search client for modification
-    print("==== Search client to modify ====")
-    modified_client_index = data.search_and_select_contact("client", client_database)
-
-    # If no client found, abort : 
-    if modified_client_index is None:
-        return
     
-    # Definition of client
-    client = client_database[modified_client_index]
-
-    # Modification of client
-    print(f"\n"
-          f"==== Modify client : {client[c.LAST_NAME]}, {client[c.FIRST_NAME]}===="
-          "\n")
-    
-    # Show available fields for modification
-    interface.show_field_info(c.CLIENT_FIELDS_INFO)
-
-    choice = input("\n"
-                   "Choose field to modify: ").strip()
-
-    if not choice.isdigit():
-        choice = data.retry_isvalid("number")
-        if choice is None:
-            return
-        
-    choice = int(choice) -1
-    
-    # User chooses field to modify and enters new value
-    if 0 <= choice < len(c.FIELDS_INFO):
-        field_chosen = c.FIELDS_INFO[choice]
-        print(f"Modification of {field_chosen.lower()}")
-        
-        if field_chosen == c.PHONE:
-            new_value = data.isvalid_phone()
-        elif field_chosen == c.EMAIL:
-            new_value = data.isvalid_email()
-        elif field_chosen == c.BIRTH or field_chosen == c.HIRING:
-            new_value = data.isvalid_date(field_chosen.lower())
-        else:
-            new_value = input(f"Enter new {field_chosen.lower()}: ")
-
-        client[field_chosen] = new_value
-
-        print()
-        print("\n"
-              f"Client modified : {client_database[modified_client_index][c.LAST_NAME]}, {client_database[modified_client_index][c.FIRST_NAME]}")
-
-        # Sort and save database
-        load.save_database(client_database)
-    
-    else:
-        print("Invalid choice. Please enter valid number.")
+    contacts.modify_contact("client", c.CLIENT_FIELDS_INFO, client_database)
 
 def delete_client(client_database):
-    """
-    Deletes single client in the client database
+    """_summary_Deletes single client in the client database
 
     Args:
         client_database (list of dict): Client database
-
-    Returns:
-        list of dict: Updated client database
     """
 
-
-    # Search client for deletion
-    print("==== Search client to delete ====")
-    selected_client_index = data.search_and_select_contact("client", client_database)
-    
-    # If no client found, abort : 
-    if selected_client_index is None:
-        return
-    
-    # User confirmation of client deletion from client database
-    client = client_database[selected_client_index]
-    confirm = input("\n"
-                    f"==== Delete client : {client[c.LAST_NAME]}, {client[c.FIRST_NAME]} ? (y/n)\n"
-                    "This operation can't be undone. Confirm?")
-    
-    if confirm != "y":
-        print("Operation cancelled.\n")
-        return
-
-    # Delete client from database
-    client_database.pop(selected_client_index)
-    print()
-    print(f"\n"
-          f"Deleted client : {client[c.LAST_NAME]}, {client[c.FIRST_NAME]}"
-          "\n")
-
-    # Sort and save database
-    load.save_database(client_database)
-
-    return client_database
+    contacts.delete_contact("client", client_database)
 
 def show_all_clients(client_database):
     """Show client list in a table.\n
---
+++ hair_manager_app/contacts_manager.py
@@ -0,0 +1,115 @@
+from . import constantes as c
+from . import app_interface as interface
+from . import data_manager as data
+from . import app_load as load
+
+def modify_contact(contact_type, FIELDS_INFO, contact_database):
+    """
+    Modifies single contact in the contact database
+
+    Args:
+        contact_type (str) : Displayed contact type in user prompts
+        FIELDS_INFO (list) : List of active fields for contact type (const)
+        contact_database (list of dict): Contact database
+    
+    Returns:
+        list of dict: Updated contact database
+    """
+
+    # Search contact for modification
+    print(f"==== Search {contact_type} to modify ====")
+    selected_contact_index = data.search_and_select_contact(contact_type, contact_database)
+
+    # If no contact found, abort : 
+    if selected_contact_index is None:
+        return
+    
+    # Definition of contact
+    contact = contact_database[selected_contact_index]
+
+    # Modification of contact
+    print(f"\n"
+          f"==== Modify {contact_type} : {contact[c.LAST_NAME]}, {contact[c.FIRST_NAME]}===="
+          "\n")
+    
+    # Show available fields for modification
+    interface.show_field_info(c.FIELDS_INFO)
+
+    choice = input("\n"
+                   "Choose field to modify: ").strip()
+
+    if not choice.isdigit():
+        choice = data.retry_isvalid("number")
+        if choice is None:
+            return
+        
+    choice = int(choice) -1
+    
+    # User chooses field to modify and enters new value
+    if 0 <= choice < len(c.FIELDS_INFO):
+        field_chosen = c.FIELDS_INFO[choice]
+        print(f"Modification of {field_chosen.lower()}")
+        
+        if field_chosen == c.PHONE:
+            new_value = data.isvalid_phone()
+        elif field_chosen == c.EMAIL:
+            new_value = data.isvalid_email()
+        elif field_chosen == c.BIRTH or field_chosen == c.HIRING:
+            new_value = data.isvalid_date(field_chosen.lower())
+        else:
+            new_value = input(f"Enter new {field_chosen.lower()}: ")
+
+        contact[field_chosen] = new_value
+
+        print()
+        print("\n"
+              f"{contact_type} modified : {contact_database[selected_contact_index][c.LAST_NAME]}, {contact_database[selected_contact_index][c.FIRST_NAME]}")
+
+        # Sort and save database
+        load.save_database(contact_database)
+    
+    else:
+        print("Invalid choice. Please enter valid number.")
+
+def delete_contact(contact_type, contact_database):
+    """
+    Deletes single contact in the contact database
+
+    Args:
+        contact_type (str) : Displayed contact type in user prompts
+        contact_database (list of dict): contact database
+
+    Returns:
+        list of dict: Updated contact database
+    """
+
+
+    # Search contact for deletion
+    print(f"==== Search {contact_type} to delete ====")
+    selected_contact_index = data.search_and_select_contact(contact_type, contact_database)
+    
+    # If no contact found, abort : 
+    if selected_contact_index is None:
+        return
+    
+    # User confirmation of contact deletion from contact database
+    contact = contact_database[selected_contact_index]
+    confirm = input("\n"
+                    f"==== Delete {contact_type} : {contact[c.LAST_NAME]}, {contact[c.FIRST_NAME]} ? (y/n)\n"
+                    "This operation can't be undone. Confirm?")
+    
+    if confirm != "y":
+        print("Operation cancelled.\n")
+        return
+
+    # Delete contact from database
+    contact_database.pop(selected_contact_index)
+    print()
+    print(f"\n"
+          f"Deleted {contact_type} : {contact[c.LAST_NAME]}, {contact[c.FIRST_NAME]}"
+          "\n")
+
+    # Sort and save database
+    load.save_database(contact_database)
+
+    return contact_database
