6b4a516d5708836a91eecb592c7c6c72f9d33a88
enripier <hp.chibi@gmail.com>
2025-06-21 16:39:14 +0200

    minor changes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import hair_manager_app.app_load as load
 import hair_manager_app.app_interface as interface
-
 import hair_manager_app.constantes as c
 
 
@@ -14,6 +13,7 @@ def main():
     load.load_database("client", c.CLIENTS_JSON_FILE, client_database)
     load.load_database("employee", c.EMPLOYEES_JSON_FILE, employee_database)
     load.load_database("employee", c.APPOINTMENTS_JSON_FILE, appointments_database)
+    
     while True:
         option = interface.hello(appointments_database, client_database, employee_database)
         if option == c.EXIT:
